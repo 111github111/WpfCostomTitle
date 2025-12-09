b0a31622551578fcefbf5f37b52a98dc648a743f
Gitea <gitea@fake.local>
2025-12-09 18:07:25 +0800

    1

--- src/AvaloniaCostomTitle/ViewModels/MainWindowViewModel.cs
+++ src/AvaloniaCostomTitle/ViewModels/MainWindowViewModel.cs
@@ -2,7 +2,7 @@
 {
     public partial class MainWindowViewModel : ViewModelBase
     {
-        public string Greeting { get; } = "Welcome to Avalonia!";
+        public string Greeting { get; } = "参考链接：https://github.com/bodong1987/AvaloniaSamples";
 
         public void CutCommand() { }
 
